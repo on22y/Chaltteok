4f59065d7ab2ab04f34e36d28596633ae4c9e6d6
on22y <lucy14lee@gmail.com>
2024-09-26 21:54:01 +0900

    제출하기 버튼 작동

--- frontend/src/components/TestComponent.js
+++ frontend/src/components/TestComponent.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios"; // Axios를 import
-import "./TestComponent.css";
-import MainBtn from "./MainBtn";
-import BoxComponent from "./BoxComponent";
-import TextComponent from "./TextComponent";
-import InputBox from "./InputBox";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // Axios를 import
+import './TestComponent.css';
+import MainBtn from './MainBtn';
+import BoxComponent from './BoxComponent';
+import TextComponent from './TextComponent';
+import InputBox from './InputBox';
 
 function TestComponent({ questionNum, num, onNext }) {
-  const [leftChat, setLeftChat] = useState("");
-  const [rightChat, setRightChat] = useState("");
-  const [chatValue, setChatValue] = useState(""); // value 값을 저장할 state
+  const [leftChat, setLeftChat] = useState('');
+  const [rightChat, setRightChat] = useState('');
+  const [chatValue, setChatValue] = useState(''); // value 값을 저장할 state
 
   useEffect(() => {
     const fetchQuestion = async () => {
       try {
-        const response = await axios.post("/Logged/test/calledQuestion", {
+        const response = await axios.post('/Logged/test/calledQuestion', {
           num: questionNum, // 여전히 DB의 문제 번호에 따라 데이터를 불러옴
         });
         setLeftChat(response.data.text1);
         setRightChat(response.data.text2);
         setChatValue(response.data.value);
       } catch (error) {
-        console.error("Error fetching the question data:", error);
+        console.error('Error fetching the question data:', error);
       }
     };
 
@@ -30,35 +30,23 @@ function TestComponent({ questionNum, num, onNext }) {
 
   return (
     <div className="testComponent">
-      <BoxComponent height="533px">
-        <div className="questionContent">
-          <TextComponent text={num} fontSize="64px" strokeWidth="1px" />
-          <div className="question">
-            다음 대화를 보고, 녹색 대화창의 <br />
-            의미를 작성하시오.
-          </div>
+      {/* <BoxComponent height="533px"> */}
+      <div className="questionContent">
+        <TextComponent text={num} fontSize="64px" strokeWidth="1px" />
+        <div className="question">
+          다음 대화를 보고, 녹색 대화창의 <br />
+          의미를 작성하시오.
         </div>
+      </div>
 
-        <div className="chatContainer">
-          <div
-            className={`chatBubble leftBubble ${
-              chatValue === "R" ? "greenBubble" : ""
-            }`}
-          >
-            {leftChat}
-          </div>
-          <div
-            className={`chatBubble rightBubble ${
-              chatValue === "L" ? "greenBubble" : ""
-            }`}
-          >
-            {rightChat}
-          </div>
-        </div>
+      <div className="chatContainer">
+        <div className={`chatBubble leftBubble ${chatValue === 'R' ? 'greenBubble' : ''}`}>{leftChat}</div>
+        <div className={`chatBubble rightBubble ${chatValue === 'L' ? 'greenBubble' : ''}`}>{rightChat}</div>
+      </div>
 
-        <InputBox text="정답을 입력해주세요." />
+      {/* <InputBox text="정답을 입력해주세요." />
       </BoxComponent>
-      <MainBtn text="다음문제" subText="못돌아가 히히" onClick={onNext} />
+      <MainBtn text="다음문제" subText="못돌아가 히히" onClick={onNext} /> */}
     </div>
   );
 }
--- frontend/src/pages/LoggedTest.js
+++ frontend/src/pages/LoggedTest.js
@@ -1,5 +1,9 @@
-import React, { useState, useEffect } from "react";
-import TestComponent from "../components/TestComponent";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import TestComponent from '../components/TestComponent';
+import BoxComponent from '../components/BoxComponent';
+import InputBox from '../components/InputBox';
+import MainBtn from '../components/MainBtn';
 
 function LoggedTest() {
   const totalQuestions = 10; // 총 10문제
@@ -8,30 +12,44 @@ function LoggedTest() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // 현재 문제 번호는 0부터 시작
   const [questionNumbers, setQuestionNumbers] = useState([]); // 문제 번호 리스트
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     // 문제를 랜덤하게 선택하지 않고 순서대로 매김
-    const sequentialQuestions = Array.from(
-      { length: totalQuestions },
-      (_, index) => index + 1
-    );
+    const sequentialQuestions = Array.from({ length: totalQuestions }, (_, index) => index + 1);
     setQuestionNumbers(sequentialQuestions); // 1부터 10까지 문제 번호 설정
   }, []);
 
   const handleNextQuestion = () => {
     if (currentQuestionIndex < totalQuestions - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1); // 다음 문제로 이동
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } // 다음 문제로 이동
+    else {
+      // 마지막 문제일 경우 '제출하기' 버튼이 작동
+      navigate('/loading');
+      setTimeout(() => {
+        navigate('/logged/type');
+      }, 3000);
     }
   };
 
   return (
     <div className="testPage">
-      {questionNumbers.length > 0 && (
-        <TestComponent
-          num={`Q${currentQuestionIndex + 1}`} // 현재 문제 번호 1부터 시작
-          questionNum={questionNumbers[currentQuestionIndex]} // 문제 데이터셋에서 순서대로 가져옴
-          onNext={handleNextQuestion} // 다음 문제로 이동
-        />
-      )}
+      <BoxComponent height="533px">
+        {questionNumbers.length > 0 && (
+          <TestComponent
+            num={`Q${currentQuestionIndex + 1}`} // 현재 문제 번호 1부터 시작
+            questionNum={questionNumbers[currentQuestionIndex]} // 문제 데이터셋에서 순서대로 가져옴
+            onNext={handleNextQuestion} // 다음 문제로 이동
+          />
+        )}
+        <InputBox text="정답을 입력해주세요." />
+      </BoxComponent>
+      <MainBtn
+        text={currentQuestionIndex === questionNumbers.length - 1 ? '제출하기' : '다음문제'}
+        subText="못돌아가 히히"
+        onClick={handleNextQuestion}
+      />
     </div>
   );
 }
