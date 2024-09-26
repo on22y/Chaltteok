021f5a01226f03aaddd035273b86eb1381d56e89
on22y <lucy14lee@gmail.com>
2024-09-26 17:53:30 +0900

    해설 부분 추가

--- frontend/src/components/AnswerComponent.js
+++ frontend/src/components/AnswerComponent.js
@@ -4,34 +4,22 @@ import './AnswerComponent.css';
 import TextComponent from './TextComponent';
 import BoxComponent from './BoxComponent';
 
-function AnswerComponent({ num, questionNum }) {
-  // 채팅 데이터를 담을 state
-  const [leftChat, setLeftChat] = useState('');
-  const [rightChat, setRightChat] = useState('');
-  const [chatValue, setChatValue] = useState(''); // value 값을 저장할 state
-
-  // 서버에서 받아오는 문제 데이터에 맞는 해설 데이터(랜덤 생성 완료)를 순차적으로 가져옴
-  useEffect(() => {
-    const fetchQuestion = async () => {
-      try {
-        const response = await axios.post('/Logged/test/calledQuestion', {
-          num: questionNum, // 순차적으로 전달받은 문제 번호로 요청
-        });
-        setLeftChat(response.data.text1);
-        setRightChat(response.data.text2);
-        setChatValue(response.data.value);
-      } catch (error) {
-        console.error('Error fetching the question data:', error);
-      }
-    };
-
-    fetchQuestion();
-  }, [questionNum]);
-
+function AnswerComponent() {
   return (
     <div className="answerComponent">
-      <TextComponent text="해설" fontsize="32px" strokeWidth="1px" colorClass="textPink" />
-      <BoxComponent></BoxComponent>
+      <TextComponent text="해설" fontSize="32px" strokeWidth="1px" colorClass="textPink" />
+      <BoxComponent width="318px" height="177px">
+        <div className="solution">
+          <div className="solution-main">핵심 단어 (단어)</div>
+          <div className="solution-sub">(해설)</div>
+
+          <div className="solution-main">
+            정답 문장 <br />
+          </div>
+
+          <div className="solution-sub">(문장)</div>
+        </div>
+      </BoxComponent>
     </div>
   );
 }
--- frontend/src/pages/IsLoggedAnswer.js
+++ frontend/src/pages/IsLoggedAnswer.js
@@ -1,48 +1,48 @@
-// import React, { useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import './Answer.css';
-// import NumList from '../components/NumList';
-// import TestComponent from '../components/TestComponent';
-// import MainBtn from '../components/MainBtn';
-// import BoxComponent from '../components/BoxComponent';
-// import trueImg from '../assets/images/trueImg.png';
-// import AnswerComponent from '../components/AnswerComponent';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Answer.css';
+import NumList from '../components/NumList';
+import TestComponent from '../components/TestComponent';
+import MainBtn from '../components/MainBtn';
+import BoxComponent from '../components/BoxComponent';
+import trueImg from '../assets/images/trueImg.png';
+import AnswerComponent from '../components/AnswerComponent';
 
-// function IsLoggedAnswer() {
-//   const navigate = useNavigate();
+function IsLoggedAnswer() {
+  const navigate = useNavigate();
 
-//   const [questionNumbers, setQuestionNumbers] = useState([]); // 랜덤 문제 번호 배열
-//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // 현재 문제 인덱스
+  const [questionNumbers, setQuestionNumbers] = useState([]); // 랜덤 문제 번호 배열
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // 현재 문제 인덱스
 
-//   useEffect(() => {
-//     // 20개의 랜덤 문제 번호를 선택
-//     const randomNumbers = [];
-//     const totalQuestions = 250; // 데이터셋 전체 개수에 맞게 수정 필요
+  useEffect(() => {
+    // 20개의 랜덤 문제 번호를 선택
+    const randomNumbers = [];
+    const totalQuestions = 250; // 데이터셋 전체 개수에 맞게 수정 필요
 
-//     while (randomNumbers.length < 20) {
-//       const randomNum = Math.floor(Math.random() * totalQuestions) + 1;
-//       if (!randomNumbers.includes(randomNum)) {
-//         randomNumbers.push(randomNum);
-//       }
-//     }
+    while (randomNumbers.length < 20) {
+      const randomNum = Math.floor(Math.random() * totalQuestions) + 1;
+      if (!randomNumbers.includes(randomNum)) {
+        randomNumbers.push(randomNum);
+      }
+    }
 
-//     setQuestionNumbers(randomNumbers);
-//   }, []);
+    setQuestionNumbers(randomNumbers);
+  }, []);
 
-//   const handleSignupClick = () => {
-//     navigate('/signuppage');
-//   };
-//   return (
-//     <div className="answerPage">
-//       <BoxComponent height="604px">
-//         <NumList totalQuestions={20} />
-//         {/* <img className="trueImg" src={trueImg} width={86} height={151} /> */}
-//         <TestComponent num={`Q${questionNumbers[currentQuestionIndex]}`} />
-//         <AnswerComponent />
-//       </BoxComponent>
-//       {/* <MainBtn text="회원가입" subText="홈으로 돌아가기" onClick={handleSignupClick} /> */}
-//     </div>
-//   );
-// }
+  const handleSignupClick = () => {
+    navigate('/signuppage');
+  };
+  return (
+    <div className="answerPage">
+      <BoxComponent height="604px">
+        <NumList totalQuestions={20} />
+        {/* <img className="trueImg" src={trueImg} width={86} height={151} /> */}
+        {/* <TestComponent num={`Q${questionNumbers[currentQuestionIndex]}`} /> */}
+        <AnswerComponent />
+      </BoxComponent>
+      <MainBtn text="회원가입" subText="홈으로 돌아가기" onClick={handleSignupClick} />
+    </div>
+  );
+}
 
-// export default IsLoggedAnswer;
+export default IsLoggedAnswer;
